b8d250709fecf69004b508f20ac32aa82b98ea50
Charvak Nangare <68916074+charvak5640@users.noreply.github.com>
2023-06-28 20:15:06 +0530

    Final commit
    this project will work on local env not yet compatible used open source infura account for ipfs

---
+++ context/constants.js
@@ -0,0 +1,6 @@
+// organizerAddress = '0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199'
+
+import voting from "./Create.json";
+// export const VotingAddress = "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199";
+export const VotingAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const VotingAddressABI = voting.abi;
